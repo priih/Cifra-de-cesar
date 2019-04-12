b68cca2a55e2d550c3ed5dd5f56ce4b7a6512d09
Priscila Marques <priscila.marques@novaandradina.org>
2019-04-11 21:30:04 -0400

    Colocando pequenas att

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-var alphabet = [
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-	'g',
-	'h',
-	'i',
-	'j',
-	'k',
-	'l',
-	'm',
-	'n',
-	'o',
-	'p',
-	'q',
-	'r',
-	's',
-	't',
-	'u',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z',
-];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+"v", "w", "x", "y", "z"];
+var letraAcento = [ " ã " , " á " , " à " , " â " , " é " , " è " , " ê " , " í " , " î " , " ì " , " ó " , " ô " , "ò " , 
+"Õ " , " ú " , " ù " , " û " , " ç " ];
+var letraSemAcento = [ " a " , " a " , " a " , " a " , " e " , " e " , " e " , " i " , " i " , " i " , " o " , " o " , 
+"o " ," O " , " u " , " u " , " u " , " c " ];
 
-var myFrase = '';
-var myGrip = '';
 
 function cipher() {
 	var palavra = (document.getElementById('exampleFormControlTextarea1').value);
